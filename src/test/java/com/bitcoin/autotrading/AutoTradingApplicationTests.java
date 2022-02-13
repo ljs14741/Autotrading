ae061102a0681288657ae8693758a8193dbc8cc6
SongChan-Gyu <zx2253@naver.com>
2022-02-13 14:41:30 +0900

    first

---
+++ src/test/java/com/bitcoin/autotrading/AutoTradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitcoin.autotrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoTradingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

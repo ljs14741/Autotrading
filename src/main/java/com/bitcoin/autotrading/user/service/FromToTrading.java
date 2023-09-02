ece85106126317260e02b3d3ad711836e6822429
송찬규 <zx2253@naver.com>
2023-09-02 21:19:30 +0900

    날짜계산

--- src/main/java/com/bitcoin/autotrading/user/service/FromToTrading.java
+++ src/main/java/com/bitcoin/autotrading/user/service/FromToTrading.java
@@ -4,22 +4,27 @@ import com.bitcoin.autotrading.candle.service.GetRsiByMinutes;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @NoArgsConstructor
 @Getter
+@Slf4j
 public class FromToTrading {
 
     @Autowired
     public GetRsiByMinutes getRsiByMinutes;
 
-    public int srt_dttm;
-    public int end_dttm;
+    public String srt_dttm;
+    public String end_dttm;
     public int state;
 
 
     @Builder
-    public FromToTrading(int srt_dttm, int end_dttm, int state){
+    public FromToTrading(String srt_dttm, String end_dttm, int state){
 
         /*
             마지막 캔들 시각 (exclusive).
@@ -37,26 +42,39 @@ public class FromToTrading {
      */
     public void BackTesting(){
 
-        int minus = this.end_dttm - this.srt_dttm;
-        int time = minus / 60 * 30; //예시 - 30분봉기준
+        try {
 
-        for (int i = 0; i < time; i++) {
-            this.Process();
+            Date to_date_end = new SimpleDateFormat("yyyyMMddHHmm").parse(this.end_dttm);
+            Date to_date_srt = new SimpleDateFormat("yyyyMMddHHmm").parse(this.srt_dttm);
+
+            long diffSec = (to_date_end.getTime() - to_date_srt.getTime()) / 1000; // 초 차이
+            long time = diffSec / (60 * 30); //예시 - 30분봉기준
+
+            log.info("diffSec =["+diffSec+"]");
+            log.info("time =["+time+"]");
+
+            for (int i = 0; i < time; i++) {
+                this.Process();
+            }
+
+        }catch (Exception e){
+            log.error(e.getMessage());
         }
     }
 
     public void Process(){
 
-        //   주문 체결 후 처리
+        //   주문 체결 후 처리 - 가잔고 테이블에 insert
         //1. 손익계산 및 잔고정리 (기간 수익을 알기 위해서) - 업비트 API는 해당 정보가 없다. 직접해야함
 
-        //2. RSI 계산(지표 계산)
+
+        //2. RSI 계산(지표 계산) -- 실제 계산
         getRsiByMinutes.main(); // RSI 계산
 
         //3. 조건 검증(지금은 RSI 값이 >30 인 로직을 넣을예정)
         int order = new ValidationOrder().validation();
 
-        //4. 매매 (주문가능조회 -> 주문)
+        //4. 매매 (주문가능조회 -> 주문) --가매매
         // order - 만들어줘 진수야
 
 

78ba7caaa6a2e0594e6a8e13208d6767f0f23a44
송찬규 <zx2253@naver.com>
2023-09-02 21:19:22 +0900

    주석

--- src/main/java/com/bitcoin/autotrading/user/service/ValidationOrder.java
+++ src/main/java/com/bitcoin/autotrading/user/service/ValidationOrder.java
@@ -9,7 +9,11 @@ import lombok.NoArgsConstructor;
 public class ValidationOrder {
 
     // 검증해서 매매가능 1, 불가 0 세팅
+    // 일단 RSI > 40?
     public int validation(){
+
+        //query or 직접 RSI 계산한 값 가져와서
+        //40 초과일시 1세팅
 
         return 0;
     }

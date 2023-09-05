14b1653ccacf21e77b6fb919d5dd219c0dc250ba
songchangyu <zx2253@naver.com>
2023-09-06 07:59:26 +0900

    제네릭변경

--- src/main/java/com/bitcoin/autotrading/common/JsonTransfer.java
+++ src/main/java/com/bitcoin/autotrading/common/JsonTransfer.java
@@ -1,5 +1,6 @@
 package com.bitcoin.autotrading.common;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.configurationprocessor.json.JSONArray;
@@ -13,6 +14,7 @@ import java.util.Map;
 
 @Slf4j
 public class JsonTransfer {
+
     public static Map<String, Object> getMapFromJSONObject(JSONObject obj) {
         if (ObjectUtils.isEmpty(obj)) {
             log.error("BAD REQUEST obj : {}", obj);
@@ -20,7 +22,12 @@ public class JsonTransfer {
         }
 
         try {
-            return new ObjectMapper().readValue(obj.toString(), Map.class);
+            /*
+            unchecked or unsafe 에러로 인해 수정
+            참고자료 : https://bjp5319.tistory.com/50
+            */
+            return new ObjectMapper().readValue(obj.toString(), new TypeReference<Map<String, Object>>() {
+            });
         } catch (Exception e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
@@ -37,6 +44,34 @@ public class JsonTransfer {
         for (int i=0; i<jsonArray.length(); i++) {
             Map<String, Object> map = getMapFromJSONObject((JSONObject)jsonArray.get(i));
             list.add(map);
+        }
+        return list;
+    }
+
+    public static <T> T getObjectFromJSONObject(JSONObject obj){
+        if (ObjectUtils.isEmpty(obj)) {
+            log.error("BAD REQUEST obj : {}", obj);
+            throw new IllegalArgumentException(String.format("BAD REQUEST obj %s", obj));
+        }
+
+        try {
+            return new ObjectMapper().readValue(obj.toString(), new TypeReference<T>() {
+            });
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static <T> List<T> getListObjectFromJSONObject(JSONArray jsonArray) throws JSONException{
+        log.info("JsonArray");
+        if (ObjectUtils.isEmpty(jsonArray)) {
+            log.error("jsonArray is null.");
+            throw new IllegalArgumentException("jsonArray is null");
+        }
+        List<T> list = new ArrayList<>();
+        for (int i=0; i<jsonArray.length(); i++) {
+            list.add(getObjectFromJSONObject((JSONObject)jsonArray.get(i)));
         }
         return list;
     }
